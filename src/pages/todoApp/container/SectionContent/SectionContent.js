9759c4f968fec0dda6825e2f98240c1cc0700e2b
hagleyson <hagleyson@gmail.com>
2021-01-21 23:38:33 -0300

    adcionandoContent

---
+++ src/pages/todoApp/container/SectionContent/SectionContent.js
@@ -0,0 +1,84 @@
+import React,{ useContext,useCallback, useState } from "react"
+import TodoContext from "./../../../../states/todos/Context"
+import Content from "./Content/Content"
+import * as todoAction from "./../../../../states/todos/actions"
+import UpdatePage from "./Modal/UpdatePage"/*
+import FilterContext from "./../../../../states/filter/Context" */
+import styles from "./sectionContent.module.css"
+/*
+function filterContent(todo,curFilter){
+    switch (curFilter) {
+        case 'all':
+            return todo    
+        case 'activityFalse':
+            return todo.filter((cur)=>{
+                return cur.status === false
+            })
+        case 'activityTrue' :
+            return todo.filter((cur)=>{
+                return cur.status === true
+            })
+        default: 
+            throw new Error()
+    }
+}*/
+function SectionContent(){
+    const {todo,dispacth} = useContext(TodoContext)
+    const handleApagar = useCallback((id)=>{
+        dispacth(todoAction.remove(id))
+    },[dispacth])    
+    const handleS = useCallback((i,s)=>{
+        dispacth(todoAction.status(i,s))        
+    },[dispacth])
+    const [curId,setCurId] = useState(null)
+    const [title,setTitle] = useState(null)
+    const openModal = useCallback((id,title)=>{
+        setCurId(id)
+        setTitle(title)        
+    },[setCurId])
+    const closeModal = useCallback(()=>{
+      setCurId(null)      
+    },[setCurId])
+    const handleUpdate = useCallback((id,title)=>{        
+        dispacth(todoAction.update(id,title))
+    },[dispacth])
+    //const {filter} = useContext(FilterContext)
+    
+    return (
+        <section className={styles.sectionC}>
+            <h1>Atividades</h1>            
+            <ul>
+                { todo.map((todoCur)=>{
+                        return (
+                            <li key={todoCur.id}>
+                            <Content 
+                                id={todoCur.id}
+                                title={todoCur.title}   
+                                statuss={todoCur.status}                     
+                                handleRemove={()=>{handleApagar(todoCur.id)}}
+                                handleStatus={handleS}
+                                handleModal={()=>{openModal(todoCur.id,todoCur.title)}}                        
+                            />  
+                            </li>       
+                            
+                        )
+                     })
+                    
+               }
+               
+               
+            </ul>
+            {curId && 
+            <UpdatePage
+                closeModal={()=>{closeModal()}}
+                id= {curId}
+                title={title}
+                valuesUpdate={handleUpdate}                
+            />
+            }
+        </section>
+    )   
+}
+
+
+export default SectionContent
--- src/pages/todoApp/container/Todos.js
+++ src/pages/todoApp/container/Todos.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Header from "./Header/Header"
+import SectionContent from "./SectionContent/SectionContent"
 function Todos(){
     return (
     <>
         <Header/>
-       
+       <SectionContent/>
     </>
     )
 }
