10b60c5ac9f9f8599a43ffb4cf104b659cb05181
hagleyson <hagleyson@gmail.com>
2021-01-21 23:29:53 -0300

    fazendo um teste

--- src/pages/todoApp/container/Todos.js
+++ src/pages/todoApp/container/Todos.js
@@ -1,13 +1,10 @@
 import React from "react"
 import Header from "./Header/Header"
-import SectionContent from "./SectionContent/SectionContent"
-import Footer from "./Footer/Footer"
 function Todos(){
     return (
     <>
         <Header/>
-        <SectionContent/>
-        <Footer/>
+       
     </>
     )
 }
